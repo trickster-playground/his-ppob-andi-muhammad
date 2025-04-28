457d7f00a2f729b817cd92041e2278253fb89e57
trickstercode-dev <trickstercode.dev@gmail.com>
2025-04-28 18:13:42 +0700

    Add Env Example

--- app/Views/layouts/auth_view.php
+++ app/Views/layouts/auth_view.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HIS PPOB</title>
+  <title>SIMS PPOB-Andi Muhammad</title>
   <!-- Load Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
 
