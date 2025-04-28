b38999d27ca3577f356bd41cbbf414502c6815d4
Andi Muhammad Fadhil Masyhun Arham <trickstercode.dev@gmail.com>
2025-04-28 19:52:24 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -45,7 +45,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
-require '/app/Config/Paths.php';
+require FCPATH . '../app/Config/Paths.php';
 
 // ^^^ Change this line if you move your application folder
 
