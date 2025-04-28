31974249da2402a4316ff9290ecfca8b561146c7
trickstercode-dev <trickstercode.dev@gmail.com>
2025-04-28 19:34:42 +0700

    Update Index.php

--- public/index.php
+++ public/index.php
@@ -8,16 +8,16 @@
 
 $minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION,
-    );
+  $message = sprintf(
+    'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
+    $minPhpVersion,
+    PHP_VERSION,
+  );
 
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo $message;
+  header('HTTP/1.1 503 Service Unavailable.', true, 503);
+  echo $message;
 
-    exit(1);
+  exit(1);
 }
 
 /*
@@ -31,7 +31,7 @@ define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
 // Ensure the current directory is pointing to the front controller's directory
 if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
+  chdir(FCPATH);
 }
 
 /*
@@ -45,7 +45,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
+require __DIR__ . '/../app/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Config\Paths();
